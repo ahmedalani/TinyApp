ecd3ba86f5807312bcd3c14fbdf7dd93a5efe91c
Ahmed Alani <s3leha@gmail.com>
2017-09-13 03:27:05 +0000

    finished up to  URL shortining part 2

--- express_server.js
+++ express_server.js
@@ -1,11 +1,27 @@
 var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+ var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -14,8 +30,41 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  // if (req.params.id )
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  console.log(shortURL)
+    let longURL = urlDatabase[shortURL];
+    res.redirect(longURL);
+
+});
+
+app.post("/urls", (req, res) => {
+  const randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+  res.redirect(301, `/urls/${randomString}`);
 });
 
 app.listen(PORT, () => {
